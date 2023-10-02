2fda7d76fa30aa5097b273105b331a8b5b80a60a
Dillon Ellis <d.ellis01.dje@gmail.com>
2023-10-02 18:25:26 +0200

    Added check to prevent loop running each time.

--- events/on_message.py
+++ events/on_message.py
@@ -27,23 +27,24 @@ async def on_message(self: client.Client, message: discord.Message):
             # Get a set of all the commands from the aliases
             commands = set(self.aliases.values())
             
-            # Loop through the commands
-            for cmd in commands:
-                # Using the similar function check if the command is similar
-                if (per := self.config.Similar(cmd, command)) >= 70:
-                    # Set the command to the similar command
-                    command = cmd
+            # Loop through the commands if command not in commands
+            if command not in commands:
+                for cmd in commands:
+                    # Using the similar function check if the command is similar
+                    if (per := self.config.Similar(cmd, command)) >= 70:
+                        # Set the command to the similar command
+                        command = cmd
 
-                    # Check if the bot can send a message
-                    if message.channel.permissions_for(message.guild.me).send_messages and per <= 75:
-                        # Send a message
-                        resp = await message.channel.send(f'> Selected command: **{command.title()}** as similarity match of {per}%')
+                        # Check if the bot can send a message
+                        if message.channel.permissions_for(message.guild.me).send_messages and per <= 75:
+                            # Send a message
+                            resp = await message.channel.send(f'> Selected command: **{command.title()}** as similarity match of {per}%')
 
-                        # Delete the message after 5 seconds
-                        await resp.delete(delay=5)
+                            # Delete the message after 5 seconds
+                            await resp.delete(delay=5)
 
-                    # Break out of the loop
-                    break
+                        # Break out of the loop
+                        break
 
 
             if Command := self.config.HandleCommand(command):
