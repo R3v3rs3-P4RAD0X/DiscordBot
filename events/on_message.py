53f2756ea0ac9171d282818e5f75e0c4106d2d7c
Dillon Ellis <d.ellis01.dje@gmail.com>
2023-10-02 18:22:04 +0200

    Added similarity checking for commands

--- events/on_message.py
+++ events/on_message.py
@@ -16,10 +16,35 @@ async def on_message(self: client.Client, message: discord.Message):
             # Get the command and arguments
             command, *args = message.content[len(config['prefix']):].strip().split(' ')
 
+            # Lowercase the command
+            command = command.lower()
+
             # Check if the command is an alias
             if command in self.aliases:
                 # Set the command to the alias
                 command = self.aliases[command]
+
+            # Get a set of all the commands from the aliases
+            commands = set(self.aliases.values())
+            
+            # Loop through the commands
+            for cmd in commands:
+                # Using the similar function check if the command is similar
+                if (per := self.config.Similar(cmd, command)) >= 70:
+                    # Set the command to the similar command
+                    command = cmd
+
+                    # Check if the bot can send a message
+                    if message.channel.permissions_for(message.guild.me).send_messages and per <= 75:
+                        # Send a message
+                        resp = await message.channel.send(f'> Selected command: **{command.title()}** as similarity match of {per}%')
+
+                        # Delete the message after 5 seconds
+                        await resp.delete(delay=5)
+
+                    # Break out of the loop
+                    break
+
 
             if Command := self.config.HandleCommand(command):
                 command = Command(message, args, self, config)
