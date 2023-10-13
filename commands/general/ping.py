ab857c30f0b0c9b60fefbb27bf9b1e03d8f6b1b8
StrangeParadox <d.ellis01.dje@gmail.com>
2023-10-13 15:18:12 +0200

    Command rewrite

--- commands/general/ping.py
+++ commands/general/ping.py
@@ -1,9 +1,9 @@
 # File: Ping
-# Description: This is the ping command, responds with a simple pong.
-#              If provided with -v for verbose it'll also include the
+# Description: Pong! The first basic command implemented into the bot, will
+#              have support for -v which makes it verbose and displays
 #              response time.
 # Author: StrangeParadox
-# Version: 0.0.1
+# Version: 0.0.2
 
 # Imports
 from components import Command
@@ -11,11 +11,23 @@ from components import Command
 
 class Ping(Command):
     """
-    This is the ping command, responds with a simple pong.
-    If provided with -v for verbose it'll also include the
+    Pong! The first basic command implemented into the bot, will
+    have support for -v which makes it verbose and displays
     response time.
     """
+    permissions = ["send_messages", "read_messages"]
 
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the class.
+        Set the variables.
+        """
         super().__init__(*args, **kwargs)
 
+
+    async def run(self):
+        """
+        Run the command.
+        """
+        # Send a message to the channel
+        await self.send("Pong!")
