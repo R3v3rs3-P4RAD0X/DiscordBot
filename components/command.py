65ab7be71d6a5b166e6b30e2878940a21743fc83
StrangeParadox <d.ellis01.dje@gmail.com>
2023-10-13 15:18:55 +0200

    Bug fixes 🪲

--- components/command.py
+++ components/command.py
@@ -20,13 +20,14 @@ class Command:
     """
 
     help: Help = Help
-    permissions: Permissions = Permissions(False, "send_messages", "read_messages")
+    permissions: ["send_messages", "read_messages"]
 
     dev = False
     hidden = False
     NSFW = False
     economy = False
     music = False
+    disabled = False
 
     def __init__(self, message: discord.Message, args: list[str], client: any = None, config: dict = {}):
         """
@@ -36,12 +37,17 @@ class Command:
         self.args = args
         self.client = client
         self.config = config
-        self.user_permissions = Permissions(self.message.author.guild_permissions.value)
-        self.bot_permissions = Permissions(self.message.guild.me.guild_permissions.value)
-        self.ratelimit = Ratelimit(self, 1, 5, type="command")
+        self.command_permissions = Permissions(False, *self.permissions)
+        self.user_permissions = Permissions(self.message.channel.permissions_for(self.message.author).value)
+        self.bot_permissions = Permissions(self.message.channel.permissions_for(self.message.guild.me).value)
+        self.ratelimit = {
+            "timeout": 5,
+            "limit": 1,
+            "ratelimit": "command"
+        }
         self.aliases = [self.__class__.__name__[::-1].lower()]
 
-        self.help = help.Help(self)
+        self.help = self.help(self)
 
 
     def create_embed(self, **kwargs) -> discord.Embed:
@@ -76,6 +82,9 @@ class Command:
         """
         Makes sure all checks related to the command pass.
         """
+        if self.disabled:
+            return False, "This command is disabled."
+
         # Check if the command is a developer command
         if self.dev and self.message.author.id not in self.config["developers"]:
             return True, "This is a developer command."
@@ -85,29 +94,31 @@ class Command:
             return True, "This command can only be used in NSFW channels."
             
         # Check if the bot has the required permissions
-        if self.bot_permissions < self.permissions:
+        if self.bot_permissions < self.command_permissions:
             # Get the permissions that the bot doesn't have
-            missing = self.permissions.missing(self.bot_permissions)
+            missing = self.command_permissions.missing(self.bot_permissions)
 
             # Check if the bot doesn't have send_messages permission
             if self.bot_permissions < Permissions(False, "send_messages"):
                 return True, "I don't have the required permissions to run this command. I need the send_messages permission."
             
             # Send the missing permissions message
-            await self.send(f"I don't have the required permissions to run this command. Missing permissions: {', '.join(missing)}")
+            resp = await self.send(f"{self.client.user.name} doesn't have the required permissions to run this command.\nMissing permissions: {', '.join(m['name'] for m in missing)}")
+            await resp.delete(delay=10)
             return True, "I don't have the required permissions to run this command."
         
         # Check if the user has the required permissions
-        if self.user_permissions < self.permissions:
+        if self.user_permissions < self.command_permissions:
             # Get the permissions that the user doesn't have
-            missing = self.permissions.missing(self.user_permissions)
+            missing = self.command_permissions.missing(self.user_permissions)
 
             # Send the missing permissions message
-            await self.send(f"You don't have the required permissions to run this command. Missing permissions: {', '.join(missing)}")
+            resp = await self.send(f"You don't have the required permissions to run this command.\nMissing permissions: {', '.join(m['name'] for m in missing)}")
+            await resp.delete(delay=10)
             return True, "You don't have the required permissions to run this command."
         
         # Check if the command is ratelimited
-        if self.ratelimit.limited(self.message.guild.id if self.ratelimit.type == "guild" or isinstance(self.ratelimit, tuple) else self.message.author.id):
+        if not self.config["ratelimiter"].check_command(self, self.message.author.id if self.ratelimit["ratelimit"] == "command" else self.message.guild.id, self.ratelimit):
             return True, "You are ratelimited."
 
         # Return False if all checks pass
@@ -122,7 +133,7 @@ class Command:
 
         if check:
             # Return the message
-            return message
+            return (message, False)
 
         # Run the command
         return await self.run()
